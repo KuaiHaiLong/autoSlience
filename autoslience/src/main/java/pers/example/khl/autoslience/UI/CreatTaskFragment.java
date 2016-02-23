629b20f45da3779cdc7ecad27c67c923037eed96
khl <kuaihailong@163.com>
2016-02-23 17:59:44 +0800

    版本 1.0 新建页面——time_choose

--- autoslience/src/main/java/pers/example/khl/autoslience/UI/CreatTaskFragment.java
+++ autoslience/src/main/java/pers/example/khl/autoslience/UI/CreatTaskFragment.java
@@ -1,109 +1,100 @@
 package pers.example.khl.autoslience.UI;
 
-import android.app.Activity;
-import android.content.Context;
-import android.net.Uri;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
 
 import pers.example.khl.autoslience.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link CreatTaskFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link CreatTaskFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class CreatTaskFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    private OnFragmentInteractionListener mListener;
-
-    public CreatTaskFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment CreatTaskFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static CreatTaskFragment newInstance(String param1, String param2) {
-        CreatTaskFragment fragment = new CreatTaskFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+public class CreatTaskFragment extends Fragment implements View.OnClickListener ,TimePickerDialog.OnTimeSetListener{
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_creat_task, container, false);
-    }
+        View view = inflater.inflate(R.layout.fragment_creat_task, container, false);
+        Button start_time = (Button)view.findViewById(R.id.start_time_choose);
+        start_time.setOnClickListener(this);
+        Button end_time = (Button)view.findViewById(R.id.end_time_choose);
+        end_time.setOnClickListener(this);
+        return view;
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
-
-    public void onAttach(Context context) {
-        super.onAttach((Activity) context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
     }
 
     @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
+    public void onClick(View v) {
+
+        Integer ViewId = v.getId();
+
+        switch(ViewId){
+            case R.id.start_time_choose:
+                timeChoose();
+                break;
+            case R.id.end_time_choose:
+                break;
+            default:
+                break;
+        }
+
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p/>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
+/*    private  String timeChoose(){
+
+        Calendar c = Calendar.getInstance();
+
+        new TimePickerDialog(this.getActivity(),
+                //绑定监听器
+                new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+
+                        *//*EditText show=(EditText)findViewById(R.id.start_time_choose);
+                        show.setText("您选择了："+hourOfDay+"时"+minute+"分");*//*
+                    }
+                }
+                //设置初始时间
+                ,c.get(Calendar.HOUR_OF_DAY)
+                ,c.get(Calendar.MINUTE)
+                //true表示采用24小时制
+                ,true).show();
+        return null;
+    }*/
+
+    private  void timeChoose(){
+
+        Calendar c = Calendar.getInstance();
+
+        new TimePickerDialog(this.getActivity(),
+                //绑定监听器
+                /*new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                        // TODO Auto-generated method stub
+                        EditText show=(EditText) view.findViewById(R.id.start_time_show);
+                        show.setText(hourOfDay+"时"+minute+"分");
+                    }
+                }*/
+                this
+                //设置初始时间
+                ,c.get(Calendar.HOUR_OF_DAY)
+                ,c.get(Calendar.MINUTE)
+                //true表示采用24小时制
+                ,true).show();
+
+    }
+
+    @Override
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+
+        Log.d("create_time_hourOfDay",""+hourOfDay);
+
     }
 }
